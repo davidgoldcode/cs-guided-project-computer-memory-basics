4e448dc61cc07c425bad5756ac34c75990117803
David Gold <davidgold@hey.com>
2020-10-29 15:01:29 -0400

    finished

--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -16,6 +16,42 @@ numbers) but have the property that they don't have a + or - sign associated
 with them. Thus they are always non-negative (zero or positive). We use uint's
 when we know the value we are counting will always be non-negative."
 """
-def hamming_weight(n):
-    # Your code here
 
+# you get normal number then convert it
+
+
+def hamming_weight(n: int) -> int:
+    # return the number of 1s in the bitwise representation of the number
+    # if we're given a normal unsized integer, how do we convert it
+    # to a bitwise representation?
+    # bitwise logical operators: '&', '|'
+    # the left & right shift operations
+    # '<<' or '>>'
+    # the '>>' allows us to shift one over to the right
+
+    # using & we have a way to check the rightmost bit of n to see if it equals 1
+
+    # FIRST EXAMPLE
+    # count = 0
+    # while n != 0:
+    #     if n & 1 == 1:
+    #         count += 1
+    #     n = n >> 1
+    # return count
+
+    # SECOND
+    # bin_representation = bin(n)
+    # counter = 0
+    # for i in range(len(bin_representation)):
+    #     if bin_representation[i] == '1':
+    #         counter += 1
+    # return counter
+
+    # THIRD
+    return bin(n).count('1')
+
+
+print(hamming_weight(10))
+print(hamming_weight(20))
+print(hamming_weight(211))
+print(hamming_weight(8))
